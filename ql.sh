24522026bbe08a9d070d257a2f2c2546c24f6cc3
andrechaumet <andrechaumetcutuli@gmail.com>
2025-01-18 01:02:29 -0300

    Added logbook read feature

--- ql.sh
+++ ql.sh
@@ -3,6 +3,25 @@
 desktop_path="$HOME/Desktop"
 logbook_path="$desktop_path/logbook.txt"
 
+write_log() {
+  timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+  text_with_timestamp="$timestamp\n$2"
+  if [ ! -f "$1" ]; then
+    echo -e "$text_with_timestamp" > "$1"
+  else
+    echo -e "\n\n$text_with_timestamp" >> "$1"
+  fi
+  echo "Text added to $1"
+}
+
+read_logs() {
+  if [ -f "$1" ]; then
+    less "$1"
+  else
+    echo "No logbook found at $1"
+  fi
+}
+
 if [ ! -d "$desktop_path" ]; then
   echo "Error: Desktop path not found: $desktop_path"
   exit 1
@@ -10,19 +29,14 @@ fi
 
 if [ $# -eq 0 ]; then
   echo "Usage: ql {text}"
+  echo "       ql -r     # To read the logbook"
   exit 1
 fi
 
-text="$*"
-timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-
-if [ ! -f "$logbook_path" ]; then
-  echo "$timestamp" > "$logbook_path"
-  echo "$text" >> "$logbook_path"
-else
-  echo -e "\n$timestamp" >> "$logbook_path"
-  echo -e "$text" >> "$logbook_path"
+if [ "$1" == "-r" ]; then
+  read_logs "$logbook_path"
+  exit 0
 fi
 
-echo "Text added to $logbook_path"
+write_log "$logbook_path" "$*"
 
